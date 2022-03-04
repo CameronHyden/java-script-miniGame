03a273edf70c154c5499ade66f0f7a85a29970da
CameronHyden <camh.hyden@gmail.comcamh.hyden@gmail.comclear>
2022-03-04 22:39:13 +0000

    player-cant-move-or-shoot-untill-they-start-game

--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-
-
-// need to get access to div's in my grid
-// and the score  board
-
 const divSquares = document.querySelectorAll(".grid div");
 const scoreBoard = document.querySelector("#result");
 const resetButton = document.querySelector("#restart-button");
@@ -22,7 +17,6 @@ const game = {
   ],
   direction: 1,
   width: 15,
-
 };
 
 let timer;
@@ -39,46 +33,52 @@ const removeAlienClass = () => {
   }
 };
 
-addShooterClass = () =>{
+addShooterClass = () => {
   divSquares[game.shooterPositionIndex].classList.add("shooter");
-}
-addShooterClass()
+};
+addShooterClass();
 
 removeShooterClass = () => {
   divSquares[game.shooterPositionIndex].classList.remove("shooter");
-} 
+};
 
-addBulletClass = () =>{
+addBulletClass = () => {
   for (let i = 0; i < game.firedBullets.length; i++) {
     divSquares[game.firedBullets[i]].classList.add("bullet");
   }
-}
-removeBulletClass = () =>{
+};
+removeBulletClass = () => {
   for (let i = 0; i < game.firedBullets.length; i++) {
     divSquares[game.firedBullets[i]].classList.remove("bullet");
   }
-}
+};
 
 //moving the shooter
 const moveShooter = (event) => {
-  removeShooterClass()
-  if (event.keyCode == "37" && game.shooterPositionIndex > 210) {
-    game.shooterPositionIndex -= 1;
-  } else {
-    if (event.keyCode == "39" && game.shooterPositionIndex < 224)
-      game.shooterPositionIndex += 1;
+  if (startGameButton.disabled == true) {
+    removeShooterClass();
+    if (event.keyCode == "37" && game.shooterPositionIndex > 210) {
+      game.shooterPositionIndex -= 1;
+    } else {
+      if (event.keyCode == "39" && game.shooterPositionIndex < 224)
+        game.shooterPositionIndex += 1;
+    }
+    addShooterClass();
   }
-  addShooterClass();
 };
 const moveShooterLeftMobile = (event) => {
-  removeShooterClass()
-  game.shooterPositionIndex -= 1;
-  addShooterClass();
+  if (startGameButton.disabled == true) {
+    removeShooterClass();
+    game.shooterPositionIndex -= 1;
+    addShooterClass();
+  }
 };
 const moveShooterRightMobile = (event) => {
-  removeShooterClass()
-  game.shooterPositionIndex += 1;
-  addShooterClass();
+  if (startGameButton.disabled == true) {
+    removeShooterClass();
+    game.shooterPositionIndex += 1;
+    addShooterClass();
+  }
 };
 
 const ifAliensHitTheEdge = () => {
@@ -112,7 +112,7 @@ const moveAliens = () => {
 };
 
 const startAndEndGame = (event) => {
-  startGameButton.disabled = true
+  startGameButton.disabled = true;
   timer = setInterval(() => {
     ifAliensHitTheEdge();
     ifAliensReachEnd();
@@ -123,27 +123,31 @@ const ifAliensReachEnd = () => {
   if (game.aliens[game.aliens.length - 1] >= 210) {
     clearInterval(timer);
     alert("GAME OVER");
+    restartTheGame();
   }
 };
 
 const shootBullet = (event) => {
-  if (event.keyCode == "38") {
+  if (startGameButton.disabled == true) {
+    if (event.keyCode == "38") {
+      game.bullet = game.shooterPositionIndex;
+      game.firedBullets.push(game.bullet);
+    }
+  }
+};
+
+const shootBulletPhone = () => {
+  if (startGameButton.disabled == true) {
     game.bullet = game.shooterPositionIndex;
     game.firedBullets.push(game.bullet);
   }
 };
 
-const shootBulletPhone = () => {
-    game.bullet = game.shooterPositionIndex;
-    game.firedBullets.push(game.bullet);
-  
-};
-
 const moveBulletArray = () => {
   for (let i = 0; i < game.firedBullets.length; i++) {
-    removeBulletClass()
+    removeBulletClass();
     game.firedBullets[i] -= game.width;
-    addBulletClass()
+    addBulletClass();
   }
 };
 setInterval(moveBulletArray, 400);
@@ -160,11 +164,19 @@ const checkIfHit = () => {
 
         game.result += 1;
         scoreBoard.innerHTML = game.result;
+        checkIfAllShotDown();
       }
     }
   }
 };
 setInterval(checkIfHit, 20);
+
+const checkIfAllShotDown = () => {
+  if (game.result === 30) {
+    alert("Nice one, you shot them all down!");
+    restartTheGame();
+  }
+};
 
 const checkIfBulletLeftBoard = () => {
   for (let i = 0; i < game.firedBullets.length; i++) {
@@ -174,30 +186,27 @@ const checkIfBulletLeftBoard = () => {
     }
   }
 };
-setInterval(checkIfBulletLeftBoard, 40);
+setInterval(checkIfBulletLeftBoard, 20);
 
-const restartTheGame = (event) =>{
-  removeAlienClass()
-  removeShooterClass()
-  removeBulletClass()
+const restartTheGame = (event) => {
+  removeAlienClass();
+  removeShooterClass();
+  removeBulletClass();
   clearInterval(timer);
   game.aliens = [
     0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 30,
     31, 32, 33, 34, 35, 36, 37, 38, 39,
-  ]
-  game.shooterPositionIndex = 217,
-  game.result = 0,
-  scoreBoard.innerHTML = game.result
-  game.bullet = null
-  game.firedBullets = []
-  startGameButton.disabled = false
-  addAlienClass()
-  addShooterClass()
-  
+  ];
+  (game.shooterPositionIndex = 217),
+    (game.result = 0),
+    (scoreBoard.innerHTML = game.result);
+  game.bullet = null;
+  game.firedBullets = [];
+  startGameButton.disabled = false;
+  addAlienClass();
+  addShooterClass();
 };
 
-// logic
-// listens to anytime a key is pressed and runs the function
 document.addEventListener("keydown", moveShooter);
 document.addEventListener("keydown", shootBullet);
 resetButton.addEventListener("click", restartTheGame);
